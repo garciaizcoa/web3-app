a0a69cba421cc38521d71c9909dc5abd1b51d96b
garciaizcoa <ricardo.garcia13@upr.edu>
2022-12-04 12:19:14 -0400

    added new mint links

--- src/components/Passport.js
+++ src/components/Passport.js
@@ -8,7 +8,7 @@ function Passport() {
             <p>Climate Resiliency Passport</p>
            <div className = 'passport__iframe'>
             <IframeResizer
-            src="https://gateway.ipfscdn.io/ipfs/Qmcine1gpZUbQ73nk7ZGCcjKBVFYXrEtqrhujXk3HDQ6Nn/erc1155.html?contract=0x92e3cb435d506Ba33fcd49A7184a50E6D8D304f4&chainId=80001&tokenId=0&rpcUrl=https%3A%2F%2Fpolygon-mumbai.g.alchemy.com%2Fv2%2FOAWQOKleuXiNq30SwezrP20LaF5TR7e5&biconomyApiKey=UhTFeppCd.e7a56bcd-65b4-4bdf-a1d4-3b15cc4f929f&biconomyApiId=ab7eef4f-22da-4669-bb81-d746ddeb8073"
+            src="https://gateway.ipfscdn.io/ipfs/Qmcine1gpZUbQ73nk7ZGCcjKBVFYXrEtqrhujXk3HDQ6Nn/erc1155.html?contract=0x750EBeAC61991dF994c6f2B470f5874B57f62383&chainId=137&tokenId=0&rpcUrl=https%3A%2F%2Fpolygon-mainnet.g.alchemy.com%2Fv2%2FvAln0F5ruuf8rysRWCOJkntIMIVsVvym&biconomyApiKey=_blmxR9zu.1faec90b-2a3e-4392-8fdf-386e2b417366&biconomyApiId=bc97dc6d-5bc9-42de-a090-c557c4acd2cc"
             style={{ width: "1px", minWidth: "100%", height: "100vh" }}
             ></IframeResizer>
             </div>
@@ -16,7 +16,7 @@ function Passport() {
             <p>Weather Scout Badge</p>
             <div className = 'passport__iframe'>
             <IframeResizer
-            src="https://gateway.ipfscdn.io/ipfs/Qmcine1gpZUbQ73nk7ZGCcjKBVFYXrEtqrhujXk3HDQ6Nn/erc1155.html?contract=0x92e3cb435d506Ba33fcd49A7184a50E6D8D304f4&chainId=80001&tokenId=0&rpcUrl=https%3A%2F%2Fpolygon-mumbai.g.alchemy.com%2Fv2%2FOAWQOKleuXiNq30SwezrP20LaF5TR7e5&biconomyApiKey=UhTFeppCd.e7a56bcd-65b4-4bdf-a1d4-3b15cc4f929f&biconomyApiId=ab7eef4f-22da-4669-bb81-d746ddeb8073"
+            src="https://gateway.ipfscdn.io/ipfs/Qmcine1gpZUbQ73nk7ZGCcjKBVFYXrEtqrhujXk3HDQ6Nn/erc1155.html?contract=0x750EBeAC61991dF994c6f2B470f5874B57f62383&chainId=137&tokenId=49&rpcUrl=https%3A%2F%2Fpolygon-mainnet.g.alchemy.com%2Fv2%2FvAln0F5ruuf8rysRWCOJkntIMIVsVvym&biconomyApiKey=_blmxR9zu.1faec90b-2a3e-4392-8fdf-386e2b417366&biconomyApiId=bc97dc6d-5bc9-42de-a090-c557c4acd2cc"
             style={{ width: "1px", minWidth: "100%", height: "100vh" }}
 
             ></IframeResizer>
